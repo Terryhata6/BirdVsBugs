0f6ea851eb1c7d0e8bdbdb5ae4693638b7124ab8
DronCorleone <48313301+DronCorleone@users.noreply.github.com>
2020-11-18 14:46:07 +0300

    FacebookSDK implementation and setup

--- Assets/Scripts/Controllers/SDKController.cs
+++ Assets/Scripts/Controllers/SDKController.cs
@@ -1,8 +1,24 @@
 ﻿using UnityEngine;
 using GameAnalyticsSDK;
+using Facebook.Unity;
 
 public class SDKController : MonoBehaviour
 {
+    void Awake()
+    {
+        if (FB.IsInitialized)
+        {
+            FB.ActivateApp();
+        }
+        else
+        {
+            //Handle FB.Init
+            FB.Init(() => {
+                FB.ActivateApp();
+            });
+        }
+    }
+
     private void Start()
     {
         GameAnalytics.Initialize();
